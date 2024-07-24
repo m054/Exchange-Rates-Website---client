42d1db6ba3d8f8f4ad931329005551362588d1b3
m054 <m0548539908@gmail.com>
2024-07-24 03:08:05 +0300

    css

--- src/Components/exchangeRates.tsx
+++ src/Components/exchangeRates.tsx
@@ -1,10 +1,9 @@
+
 import React from "react";
-// import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import DropDownMenu from 'material-ui/DropDownMenu';
 import { MenuItem } from "@material-ui/core";
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { ExchangeRatesTable } from "./exchangeRatesTable";
-
+import './exchangeRates.css';
 
 export const ExchangeRates: React.FC = () => {
 
@@ -17,8 +16,8 @@ export const ExchangeRates: React.FC = () => {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    const style1 = { width: '9vw', height: '30px' };
-    const style2 = { backgroundColor: '#D200FF', borderBlockEnd: '1px solid white' };
+    const style1 = { width: '15vw', height: '50px',border: '3px solid black'};
+     const style2 = { backgroundColor: 'white', borderBlockEnd: '1px solid white',borderBottom: '1px solid gray'};
 
 
 
@@ -34,7 +33,8 @@ export const ExchangeRates: React.FC = () => {
     return (
 
         //Displaying the list in a dropdown control
-        <div>
+        <div className="exchangeRatse">
+            <h1>Exchange Rates</h1>
             <Select
                 value={Currency}
                 onChange={handleChangeCurrency}
@@ -43,13 +43,74 @@ export const ExchangeRates: React.FC = () => {
                 style={style1}
                 MenuProps={MenuProps}
                 variant='outlined'
-                color='secondary' >
+                color='secondary' 
+                
+                >
                 {arr.map((element: any) =>
                     <MenuItem value={element} style={style2}>{element}</MenuItem>
                 )}
             </Select>
-            <div>{Currency !== '' && <ExchangeRatesTable selectedCurrency={Currency} />}</div>
+            <div className="table">{Currency !== '' && <ExchangeRatesTable selectedCurrency={Currency} />}</div>
         </div>
 
     );
 }
+
+
+
+
+// import React from "react";
+// // import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+// // import DropDownMenu from 'material-ui/DropDownMenu';
+// import { MenuItem } from "@material-ui/core";
+// import Select, { SelectChangeEvent } from '@mui/material/Select';
+// import { ExchangeRatesTable } from "./exchangeRatesTable";
+
+
+// export const ExchangeRates: React.FC = () => {
+
+//     const [arr, setArr] = React.useState([]);
+//     //Retrieving a list of coins from the server
+//     React.useEffect(() => {
+//         fetch('https://localhost:7203/api/ExchangeRatse/GetAll')
+//             .then(response => response.json())
+//             .then(data => setArr(data))
+//             .catch(error => console.error('Error fetching data:', error));
+//     }, []);
+
+//     const style1 = { width: '9vw', height: '30px' };
+//     const style2 = { backgroundColor: '#D200FF', borderBlockEnd: '1px solid white' };
+
+
+
+//     const [Currency, setCurrency] = React.useState('');
+//     const ITEM_HEIGHT = 48;
+//     const ITEM_PADDING_TOP = 8;
+//     const MenuProps = { PaperProps: { style: { maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP, width: 150 } } };
+
+//     const handleChangeCurrency = (event: SelectChangeEvent) => {
+//         setCurrency(event.target.value);
+//     };
+
+//     return (
+
+//         //Displaying the list in a dropdown control
+//         <div>
+//             <Select
+//                 value={Currency}
+//                 onChange={handleChangeCurrency}
+//                 displayEmpty
+//                 inputProps={{ 'aria-label': 'Without label' }}
+//                 style={style1}
+//                 MenuProps={MenuProps}
+//                 variant='outlined'
+//                 color='secondary' >
+//                 {arr.map((element: any) =>
+//                     <MenuItem value={element} style={style2}>{element}</MenuItem>
+//                 )}
+//             </Select>
+//             <div>{Currency !== '' && <ExchangeRatesTable selectedCurrency={Currency} />}</div>
+//         </div>
+
+//     );
+// }
